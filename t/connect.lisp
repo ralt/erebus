973b3af3e0e1f7bdb6dd660334616c8a408969b3
Florian Margaine <florian@margaine.com>
2025-12-30 23:17:06 +0100

    Start openvpn server with the config we need

--- t/connect.lisp
+++ t/connect.lisp
@@ -2,58 +2,46 @@
 
 (def-suite* erebus/connect :in erebus)
 
-(test connect-to-vpn
+(test connect-to-vpn-static-key-udp
   (with-docker-container (name
                           folder
                           vpn-local-port
                           (lambda (name folder)
                             (run-in-container
                              name
-                             "cat > /etc/openvpn/openvpn.conf <<EOF
-ca /etc/openvpn/pki/ca.crt
+                             "cd /etc/openvpn
+openvpn --genkey --secret static.key
+chmod 777 static.key
+rm -rf ccd/ crl.pem pki/ # delete those or ovpn_run will try to use them
 
+cat > /etc/openvpn/openvpn.conf <<EOF
+server 192.168.255.0 255.255.255.0
+verb 3
+keepalive 10 60
+persist-tun
+secret static.key
+
+proto udp
+# Rely on Docker to do port mapping, internally always 1194
 port 1194
-proto tcp
 dev tun0
+status /tmp/openvpn-status.log
 
 user nobody
 group nogroup
 comp-lzo no
 
-mode server
-server 192.168.255.0 255.255.255.0
+### Route Configurations Below
 route 192.168.254.0 255.255.255.0
-
-# --- Disable client authentication ---
-verify-client-cert none
-auth-user-pass-verify /bin/true via-file
-username-as-common-name
-auth none
-duplicate-cn
-
-# --- Crypto (still required internally) ---
-dh none
-cipher AES-256-GCM
-ncp-disable
-keysize 256
-
-# --- Control channel ---
-keepalive 10 60
-persist-key
-persist-tun
 
 # --- Push minimal config ---
 push \"route 192.168.254.0 255.255.255.0\"
 push \"block-outside-dns\"
 push \"dhcp-option DNS 8.8.8.8\"
 push \"comp-lzo no\"
-
-# --- Logging ---
-verb 4
-log /etc/openvpn/openvpn.log
-status /tmp/openvpn.status
+EOF
 ")))
-    (let ((socket (usocket:socket-connect "localhost" vpn-local-port)))
+    (let ((socket (usocket:socket-connect "localhost" vpn-local-port :protocol :datagram)))
       (usocket:socket-close socket)
       ;; just checking we didn't raise any conditions
       (is (= 1 1)))))
